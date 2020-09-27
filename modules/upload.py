4b0937746b0a31270f945b01b053e120be18691f
AndrielFR <andrielkogama2@gmail.com>
2020-09-27 18:01:11 -0300

    vitao: Upload: fix download path and some improvements

--- modules/upload.py
+++ modules/upload.py
@@ -3,10 +3,15 @@ from modules.Module import Module
 from requests import exceptions, get, post
 
 import random
+import os
+
+# Configuration
+from modules.Config import Configuration
 
 class upload(Module):
     def __init__(self, update, bot, command, message):
         self.bot = bot
+        self.config = Configuration()
         
         self._message = update.message
         
@@ -19,43 +24,42 @@ class upload(Module):
         tx = self._message.reply_to_message
         message = self.message
         final_url = ''
+        reply = 0
         
         self._send_action('typing')
         
         if message:
             pass
         elif tx:
-            if tx.voice or tx.photo or tx.video:
+            if tx.voice or tx.photo or tx.video or (tx.document and tx.document.file_name.endswith(('.zip', '.gif', '.mp3', '.mp4', '.tgz', '.tg'))):
                 error = '***Formato de arquivo não suportado.***'
                 self._send_message(error, self._message.message_id)
                 return [False, error]
             if tx.document:
-                if tx.document.file_name.endswith(('.zip', '.gif', '.mp4', '.tgz', '.tg')):
-                    error = '***Formato de arquivo não suportado.***'
-                    self._send_message(error, self._message.message_id)
-                    return [False, error]
-                    
-                file_name = '{0}-{1}'.format(tx.document.file_name, random.randint(0, 9999))
-                
+                file_name = '{1}-{0}'.format(tx.document.file_name, random.randint(0, 9999))
+                 
                 try:
                     file = self._get_file(tx.document.file_id)
-                    file.download(custom_path='{0}{1}'.format(self.DOWNLOAD_DIR, file_name))
+                    file.download(custom_path='{0}{1}'.format(self.config.download_dir, file_name))
                 except:
                     error = '***Arquivo muito pesado.***'
                     self._send_message(error, self._message.message_id)
                     return [False, error]
                     
                 m_l = None
-                with open(self.DOWNLOAD_DIR+file_name, 'rb') as fd:
+                with open(self.config.download_dir+file_name, 'rb') as fd:
                     m_l = fd.readlines()
                     
                 message = ''
                 for m in m_l:
                     message += m.decode('utf-8')
                     
-                os.remove(self.DOWNLOAD_DIR+file_name)
+                os.remove(self.config.download_dir+file_name)
             else:
                 message = tx.text
+                
+        if not self._delete_message(self._message.message_id):
+            reply = self._message.message_id
         
         # del.dog
         dFail = False
@@ -77,11 +81,8 @@ class upload(Module):
             else:
                 r_te = "`Falha ao se conectar com o del.dog`"
                 dFail = True
-             
+
             if tx:
-                try:
-                    self._delete_message(self._message.message_id)
-                except:
-                    pass
-            self._send_message(r_te, tx.message_id)
+                reply = tx.message_id
+            self._send_message(r_te, reply)
             return[True]
